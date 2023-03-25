5cb279325293df525ea7a7ee8e35741e9072600a
RobertFrentz <30467166+RobertFrentz@users.noreply.github.com>
2023-03-25 19:18:34 +0200

    Added dummy topic kafka producer application

---
+++ StockProducer/src/main/java/org/example/KafkaProducerExample.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class KafkaProducerExample {
+    public static void main(String[] args) {
+        String topicName = "my-topic-test";
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "kafka:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for (int i = 0; i < 10; i++) {
+            String key = "key-" + i;
+            String value = "value-" + i;
+            ProducerRecord<String, String> record = new ProducerRecord<>(topicName, key, value);
+            producer.send(record);
+        }
+        producer.close();
+    }
+}
